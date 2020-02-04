1b6d3b02e22788efac6342f9dc9e818bb9568e4e
Allison Hancock <aahancoc@umich.edu>
2020-02-04 14:46:44 -0500

    Add support for parsing strings with long ops
    Also fixed testcases to work on my system.

--- src/ShuntingYard.js
+++ src/ShuntingYard.js
@@ -3,142 +3,158 @@
  * look it up: http://en.wikipedia.org/wiki/Shunting-yard_algorithm
  *
  * Copyright 2013, Georg Tavonius
+ *           2020, Allison Hancock <aahancoc@umich.edu>
  * Licensed under the MIT license.
  *
- * @version 2.0.0
+ * @version 3.0.0
  *
  * @author Georg Tavonius a.k.a. Calamari (http://github.com/Calamari)
  * @homepage http://github.com/Calamari/shunting-yard.js
  */
 import Operator from './Operator'
-import Function from './Function'
 
 export default class ShuntingYard {
-  constructor (options) {
-    this.operators = options.operators || {
-      '+': new Operator('+', 2, 'left', 2, function(a, b) { return a + b }),
-      '-': new Operator('-', 2, 'left', 2, function(a, b) { return a - b }),
-      '*': new Operator('*', 3, 'left', 2, function(a, b) { return a * b }),
-      '/': new Operator('/', 3, 'left', 2, function(a, b) { return a / b }),
-      '^': new Operator('^', 4, 'right', 2, function(a, b) { return Math.pow(a, b) })
-    };
-    this.functions = options.functions || {};
-    this.parse_raw = options.parse_raw || false;
-  }
+	constructor (options = {}) {
+		this.operators = options.operators || {
+			'+': new Operator('+', 2, 'left', 2, function(a, b) { return a + b }),
+			'-': new Operator('-', 2, 'left', 2, function(a, b) { return a - b }),
+			'*': new Operator('*', 3, 'left', 2, function(a, b) { return a * b }),
+			'/': new Operator('/', 3, 'left', 2, function(a, b) { return a / b }),
+			'^': new Operator('^', 4, 'right', 2, function(a, b) { return Math.pow(a, b) })
+		};
+		this.functions = options.functions || {};
+		this.parse_raw = options.parse_raw || false;
+	}
 
-  addFunction (key, func) {
-    if (this.functions[key]) {
-      console.warn(`Function ${key} already exists`)
-    }
-    this.functions[key] = func
-  }
+	addFunction (key, func) {
+		if (this.functions[key]) {
+			console.warn(`Function ${key} already exists`)
+		}
+		this.functions[key] = func
+	}
 
-  addOperator (key, operator) {
-    if (this.operators[key]) {
-      console.warn(`Operator ${key} already exists`)
-    }
-    this.operators[key] = operator
-  }
+	addOperator (key, operator) {
+		if (this.operators[key]) {
+			console.warn(`Operator ${key} already exists`)
+		}
+		this.operators[key] = operator
+	}
 
-  parse (str) {
-    let output = []
-    let stack = []
-    let sign
-    let lastToken
-    let token
+	parse (str) {
+		let output = []
+		let stack = []
+		let sign
+		let lastToken
+		let token
+		let i = 0;
 
-    for (let i=0, l=str.length; i<l; ++i) {
-      token = str[i]
-      if (token === ' ') {
-        continue // do nothing with spaces
-      }
-      if (sign) {
-        token = sign += token
-        sign = null
-      }
-      if (this.isLeftPara(token)) {
-        stack.push(token)
-      } else if (this.isFunction(token)) {
-        stack.push(token)
-      } else if (this.isRightPara(token)) {
-        let operator
-        while ((operator = stack.pop()) && !this.isLeftPara(operator)) {
-          if (!this.isFunction(operator)) {
-            output.push(operator)
-          }
-        }
-        if (typeof operator === 'undefined') {
-          return null // to many closing paranthesis
-        }
-      } else if (this.isOperator(token)) {
-        if (!lastToken || lastToken === '(') {
-          sign = token
-          continue
-        }
-        while (stack.length) {
-          const thisOperator = this.operators[token]
-          const operator = this.operators[stack[stack.length-1]]
-          if (!operator || !thisOperator) { break }
-          if ((thisOperator.leftAssoc() && thisOperator.lessThenEqual(operator)) || thisOperator.lessThen(operator)) {
-            output.push(stack.pop())
-          } else {
-            break
-          }
-        }
-        stack.push(token)
-      } else {
-        if (!lastToken || this.isLeftPara(lastToken) || this.isOperator(lastToken)) {
-          output.push(token)
-        } else {
-          output[output.length - 1] += token
-        }
-      }
-      lastToken = token
-    }
+		// List of tokens that are operators or functions
+		const op_tokens =
+			Object.keys(this.operators)
+			.concat(Object.keys(this.functions));
 
-    while (stack.length) {
-      token = stack.pop()
-      if (this.isLeftPara(token)) {
-        return null // to many opening paranthesis
-      }
-      output.push(token)
-    }
+		while (i < str.length) {
+			token = str[i];
+			// Find the corresponding operator
+			for (let nit of op_tokens) {
+				// If pre-tokenized, skip this step
+				if (typeof(str) !== "string") { break; }
+				// If token matches, make that the new token
+				if (str.slice(i).startsWith(nit)) { token = nit; break; }
+			}
+			// Increment i by the token size
+			i += (typeof(str) === "string") ? token.length : 1;
+			// Skip if token is actually just a space
+			if (token === ' ') { continue; }
 
-    return output
-  }
+			if (sign) {
+				token = sign += token
+				sign = null
+			}
 
-  resolveRpn (arr) {
-    let stack = []
+			if (this.isLeftPara(token)) {
+				stack.push(token)
+			} else if (this.isFunction(token)) {
+				stack.push(token)
+			} else if (this.isRightPara(token)) {
+				let operator;
+				while ((operator = stack.pop()) && !this.isLeftPara(operator)) {
+					if (!this.isFunction(operator)) {
+						output.push(operator)
+					}
+				}
+				if (typeof operator === 'undefined') {
+					throw SyntaxError("Too many closing parenthesis");
+				}
+			} else if (this.isOperator(token)) {
+				if (!lastToken || lastToken === '(') {
+					sign = token
+					continue
+				}
+				while (stack.length) {
+					const thisOperator = this.operators[token]
+					const operator = this.operators[stack[stack.length-1]]
+					if (!operator || !thisOperator) { break }
+					if ((thisOperator.leftAssoc() && thisOperator.lessThenEqual(operator)) || thisOperator.lessThen(operator)) {
+						output.push(stack.pop())
+					} else {
+						break
+					}
+				}
+				stack.push(token)
+			} else {
+				if (!lastToken || this.isLeftPara(lastToken) || this.isOperator(lastToken)) {
+					output.push(token)
+				} else {
+					output[output.length - 1] += token
+				}
+			}
+			lastToken = token
+		}
 
-    for (let i=0, l=arr.length; i<l; ++i) {
-      const op = this.operators[arr[i]] || this.functions[arr[i]]
-      if (op) {
-        stack.push(op.method.apply(this, stack.splice(-op.params)))
-      } else {
-        stack.push(this.parse_raw ? arr[i] : parseFloat(arr[i]))
-      }
-    }
+		while (stack.length) {
+			token = stack.pop()
+			if (this.isLeftPara(token)) {
+				throw SyntaxError("Too many opening parenthesis");
+			}
+			output.push(token)
+		}
 
-    return stack[0]
-  }
+		return output
+	}
 
-  resolve (str) {
-    return this.resolveRpn(this.parse(str))
-  }
+	resolveRpn (arr) {
+		let stack = []
 
-  isLeftPara (token) {
-    return token === '('
-  }
+		for (let i=0, l=arr.length; i<l; ++i) {
+			const op = this.operators[arr[i]] || this.functions[arr[i]]
+			if (op) {
+				stack.push(op.method.apply(this, stack.splice(-op.params)))
+			} else {
+				stack.push(this.parse_raw ? arr[i] : parseFloat(arr[i]))
+			}
+		}
 
-  isRightPara (token) {
-    return token === ')'
-  }
+		return stack[0]
+	}
 
-  isOperator (token) {
-    return Object.keys(this.operators).indexOf(token) !== -1
-  }
+	resolve (str) {
+		return this.resolveRpn(this.parse(str))
+	}
 
-  isFunction (token) {
-    return Object.keys(this.functions).indexOf(token) !== -1
-  }
+	isLeftPara (token) {
+		return token === '('
+	}
+
+	isRightPara (token) {
+		return token === ')'
+	}
+
+	isOperator (token) {
+		return Object.keys(this.operators).indexOf(token) !== -1
+	}
+
+	isFunction (token) {
+		return Object.keys(this.functions).indexOf(token) !== -1
+	}
 }
--- test/index.js
+++ test/index.js
@@ -1,8 +1,137 @@
-// const requireAll = (requireContext) => { requireContext.keys().map(requireContext); };
+import { expect} from 'chai';
 
-// // requireAll(require.context('spec/helpers/', true, /\.js$/));
-// requireAll(require.context('./', true, /[sS]pec\.js$/));
-// var testsContext = require.context(".", true, /_test$/);
-// testsContext.keys().forEach(testsContext);
+import ShuntingYard, { Operator, Function } from '../src/index';
 
-require('./ShuntingYard_test');
+describe("ShuntingYard", function() {
+  var shuntingYard;
+
+  beforeEach(function() {
+    shuntingYard = new ShuntingYard();
+  });
+
+  const testCase = function(key, val) {
+    it('"' + key + '" => ' + val.join(' '), function() {
+      expect(shuntingYard.parse(key)).to.eql(val);
+    });
+  };
+
+  const testResolveCase = function(key, val){
+    it('"' + key + '" => ' + val, function() {
+      expect(shuntingYard.resolve(key)).to.eql(val);
+    });
+  };
+
+  it('returns an Array', function() {
+    expect(shuntingYard.parse('').constructor).to.eql(Array);
+  });
+
+  describe("resolves strings", function() {
+    var positiveTest = {
+      '10+3': ['10', '3', '+'],
+      '10 - 3': ['10', '3', '-'],
+      '10.5-3.25': ['10.5', '3.25', '-'],
+      '42*0.1': ['42', '0.1', '*'],
+      '42/0.1': ['42', '0.1', '/'],
+      '-42': ['-42'],
+      '+23': ['+23'],
+      '12^2': ['12', '2', '^'],
+      '-2 * 4': ['-2', '4', '*'],
+      '(10+3)': ['10', '3', '+'],
+      '10+3*3': ['10', '3', '3', '*', '+'],
+      '(-10+3)': ['-10', '3', '+'],
+      '3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3': ['3', '4', '2', '*', '1', '5', '-', '2', '3', '^', '^', '/', '+'],
+      '-1 * (-4 + 2)': ['-1', '-4', '2', '+', '*']
+    };
+    for (var key in positiveTest) {
+      testCase(key, positiveTest[key]);
+    }
+  });
+
+  describe("resolves this tokenized array:", function() {
+    var positiveTest = {
+      '10 + 3': ['10', '3', '+'],
+      '10 - 3': ['10', '3', '-'],
+      '10.5 - 3.25': ['10.5', '3.25', '-'],
+      '42 * 0.1': ['42', '0.1', '*'],
+      '42 / 0.1': ['42', '0.1', '/'],
+      '-42': ['-42'],
+      '+23': ['+23'],
+      '12 ^ 2': ['12', '2', '^'],
+      '-2 * 4': ['-2', '4', '*'],
+      '( 10 + 3 )': ['10', '3', '+'],
+      '10 + 3 * 3': ['10', '3', '3', '*', '+'],
+      '( -10 + 3 )': ['-10', '3', '+'],
+      '3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3': ['3', '4', '2', '*', '1', '5', '-', '2', '3', '^', '^', '/', '+'],
+      '-1 * ( -4 + 2 )': ['-1', '-4', '2', '+', '*']
+    };
+    for (var key in positiveTest) {
+      testCase(key.split(' '), positiveTest[key]);
+    }
+  });
+
+  describe('resolves some calculations', function() {
+    var positiveTest = {
+      '10 + 3': 13,
+      '10 - 3': 7,
+      '10.5 - 3.25': 7.25,
+      '42 * 0.1': 4.2,
+      '-42': -42,
+      '+23': 23,
+      '12 ^ 2': 144,
+      '10 + 3 * 3': 19,
+      '(10 + 3) * 3': 39
+    };
+    for (var key in positiveTest) {
+      testResolveCase(key, positiveTest[key]);
+    }
+  });
+
+  it('throws SyntaxError on too many closing parentheses', function() {
+    expect(shuntingYard.parse.bind(shuntingYard, '1*(3+4))')).to.throw(SyntaxError);
+  });
+
+  it('throws SyntaxError if too many closing parentheses', function() {
+    expect(shuntingYard.parse.bind(shuntingYard, '1*((3+4)')).to.throw(SyntaxError);
+  });
+
+  describe("when having functions", function() {
+    beforeEach(function() {
+      shuntingYard.addFunction('sqrt', new Function('sqrt', 1, function(a) { return Math.sqrt(a); }))
+    });
+
+    it('parses those functions', function() {
+      expect(shuntingYard.parse([1, '*', 'sqrt', '(', 3, '+', 6, ')'])).to.eql([1, 3, 6, '+', 'sqrt', '*']);
+      expect(shuntingYard.parse("1 * sqrt(3 + 6)")).to.eql(['1', '3', '6', '+', 'sqrt', '*']);
+    });
+
+    it('resolves those functions', function() {
+      expect(shuntingYard.resolve([1, '*', 'sqrt', '(', 3, '+', 6, ')'])).to.eql(3);
+      expect(shuntingYard.resolve("1 * sqrt(3 + 6)")).to.eql(3);
+    });
+
+    it('resolves created RPN', function() {
+      expect(shuntingYard.resolveRpn([1, 3, 6, '+', 'sqrt', '*'])).to.eql(3);
+    });
+
+    describe("when adding some custom operators", function() {
+      beforeEach(function() {
+        shuntingYard.addOperator('add', new Operator('add', 2, 'left', 2, function(a, b) { return a + b; }))
+        shuntingYard.addOperator('multiply', new Operator('multiply', 2, 'left', 2, function(a, b) { return a * b; }));
+      });
+
+      it('it parses those custom operators', function() {
+        expect(shuntingYard.parse(['2','multiply','(','3','add','4',')'])).to.eql(['2', '3', '4', 'add', 'multiply']);
+        expect(shuntingYard.parse('2 multiply(3 add 4)')).to.eql(['2', '3', '4', 'add', 'multiply']);
+      });
+
+      it('it resolves those custom operators', function() {
+        expect(shuntingYard.resolve(['2','multiply','(','3','add','4',')'])).to.eql(14);
+        expect(shuntingYard.resolve('2 multiply(3 add 4)')).to.eql(14);
+      });
+
+      it('resolves cerated RPN', function() {
+        expect(shuntingYard.resolveRpn(['2', '3', '4', 'add', 'multiply'])).to.eql(14);
+      });
+    });
+  });
+});
